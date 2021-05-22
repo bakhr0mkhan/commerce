d623c41c3f5883091c5fea6d4300c19e99a22a51
Bakhromkhan <67605887+bakhr0mkhan@users.noreply.github.com>
2021-05-22 12:40:15 +0500

    done navbar

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState, FC } from "react";
 import Lottie from "react-lottie";
-import { FaCartPlus } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 import { useCommerceContext, useFirebaseContext } from "../../context/index";
 import "./styles.css";
-import { localCategories } from "../../assets/data/index";
-import { Header } from "../../components";
+import { localCategories } from "../../assets/data";
 import commerceLoading from "../../assets/animations/commerceLoading.json";
+//@ts-ignore
+import { useHistory } from "react-router-dom";
+import firebase from "firebase/app";
 
 const Dashboard: FC = () => {
   //@ts-ignore
   const { commerce } = useCommerceContext();
   //@ts-ignore
   const { currentUser } = useFirebaseContext();
+  const auth = firebase.auth();
+
+  const history = useHistory();
 
   const [categories, setCategories] = useState<any>();
   const [products, setProducts] = useState<any>();
@@ -21,6 +26,7 @@ const Dashboard: FC = () => {
   useEffect(() => {
     console.log("auth user", currentUser);
     fetchProducts();
+    console.log("l c", localCategories);
     return () => setProducts({});
   }, []);
 
@@ -76,6 +82,13 @@ const Dashboard: FC = () => {
     }
   };
 
+  const signOut = async () => {
+    await auth
+      .signOut()
+      .then((res) => console.log("signed out", res))
+      .catch((err) => console.log("could not sign out", err));
+  };
+
   if (loading) {
     return (
       <Lottie
@@ -96,80 +109,111 @@ const Dashboard: FC = () => {
   }
 
   return (
-    <div className="container">
-      <Header />
-      <div className="mainCon">
-        <div className="leftCon">
-          <div className="">
-            <p className="heading3">Explore</p>
-          </div>
-          <div className="categoriesCon">
-            {localCategories?.map((category) => (
-              <div
-                key={category.name}
-                className="categorySingleCon"
-                style={{
-                  backgroundColor:
-                    category.name === selectedCategory.name
-                      ? "#ebfaef"
-                      : "white",
-                }}
-                onClick={() => setSelectedCategory(category)}
-              >
-                <div className="categoryIconCon">
-                  <img src={category.icon} className="btnIcon" alt="Icon" />
-                </div>
-                <div className="categoryNameCon">
-                  <button className="categoryName">{category.name}</button>
-                </div>
-              </div>
-            ))}
-          </div>
+    <div className="mainCon">
+      <div className="navbar">
+        <div className="navbarLeft">
+          <h3 className="branName">Kamazon</h3>
         </div>
-        <div className="rightCon">
-          <div className="categoryHeaderCon">
-            <div className="subContainer">
-              <div>
-                <img
-                  src={selectedCategory.icon}
-                  className="catIcon"
-                  alt="Icon"
-                />
-              </div>
-              <div>
-                <p className="heading3">{selectedCategory.name}</p>
-              </div>
-            </div>
-            <div>
-              <div>
-                <button className="filterBtn">Filter</button>
-              </div>
-            </div>
+        <div className="navbarRight">
+          <div className="cartIconCon">
+            <button
+              className="cartBtn"
+              onClick={() => {
+                history.push("/cart");
+              }}
+            >
+              <FaShoppingCart className="cartIcon" />
+            </button>
           </div>
-
-          <div className="prodsCon">
-            {products?.map((prod: any) => (
-              <div key={prod.id} className="prodCon">
-                <div className="prodImgCon">
-                  <img src={prod.media.source} className="prodImg" />
-                </div>
-                <div className="prodInfoCon">
-                  <div className="prodNameContainer">
-                    <p className="prodName">{prod.name.slice(0, 18)}...</p>
-                  </div>
-                  <button
-                    className="addToCartCon"
-                    onClick={() => addToCart(prod.id)}
-                  >
-                    <FaCartPlus className="addToCartIcon" />
-                  </button>
-                </div>
-              </div>
-            ))}
+          <div className="authIconCon">
+            <button
+              className="authBtn"
+              onClick={() => {
+                currentUser ? signOut() : history.push("/login");
+              }}
+            >
+              {currentUser ? "Log out" : "Login"}
+            </button>
           </div>
         </div>
       </div>
     </div>
+
+    //   <div className="container">
+    //     <Header />
+    //     <div className="mainCon">
+    //       <div className="leftCon">
+    //         <div className="">
+    //           <p className="heading3">Explore</p>
+    //         </div>
+    //         <div className="categoriesCon">
+    //           {localCategories?.map((category) => (
+    //             <div
+    //               key={category.name}
+    //               className="categorySingleCon"
+    //               style={{
+    //                 backgroundColor:
+    //                   category.name === selectedCategory.name
+    //                     ? "#ebfaef"
+    //                     : "white",
+    //               }}
+    //               onClick={() => setSelectedCategory(category)}
+    //             >
+    //               <div className="categoryIconCon">
+    //                 <img src={category.icon} className="btnIcon" alt="Icon" />
+    //               </div>
+    //               <div className="categoryNameCon">
+    //                 <button className="categoryName">{category.name}</button>
+    //               </div>
+    //             </div>
+    //           ))}
+    //         </div>
+    //       </div>
+    //       <div className="rightCon">
+    //         <div className="categoryHeaderCon">
+    //           <div className="subContainer">
+    //             <div>
+    //               <img
+    //                 src={selectedCategory.icon}
+    //                 className="catIcon"
+    //                 alt="Icon"
+    //               />
+    //             </div>
+    //             <div>
+    //               <p className="heading3">{selectedCategory.name}</p>
+    //             </div>
+    //           </div>
+    //           <div>
+    //             <div>
+    //               <button className="filterBtn">Filter</button>
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //         <div className="prodsCon">
+    //           {products?.map((prod: any) => (
+    //             <div key={prod.id} className="prodCon">
+    //               <div className="prodImgCon">
+    //                 <img src={prod.media.source} className="prodImg" />
+    //               </div>
+    //               <div className="prodInfoCon">
+    //                 <div className="prodNameContainer">
+    //                   <p className="prodName">{prod.name.slice(0, 18)}...</p>
+    //                 </div>
+    //                 <button
+    //                   className="addToCartCon"
+    //                   onClick={() => addToCart(prod.id)}
+    //                 >
+    //                   <FaCartPlus className="addToCartIcon" />
+    //                 </button>
+    //               </div>
+    //             </div>
+    //           ))}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //
   );
 };
 
