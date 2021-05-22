eababbebe641a143aa87b05a78ac2b19c78ab6f0
Bakhromkhan <67605887+bakhr0mkhan@users.noreply.github.com>
2021-05-22 20:45:16 +0500

    done register

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -5,11 +5,10 @@ import { useForm } from "react-hook-form";
 import { Redirect, useHistory } from "react-router-dom";
 import Lottie from "react-lottie";
 
-import FBIcon from "../../assets/icons/fb.png";
-import GoogleIcon from "../../assets/icons/google.png";
 import loginLoading from "../../assets/animations/loginLoading.json";
 import registration0 from "../../assets/animations/registration0.json";
 import registration1 from "../../assets/animations/registration1.json";
+import GoogleIcon from "../../assets/icons/google.png";
 
 interface Props {}
 
@@ -89,88 +88,163 @@ const Register = (props: Props) => {
     );
   }
   return (
-    <div className="mainConLogin">
-      <div className="sidebar left">
-        <Lottie
-          options={{
-            loop: true,
-            autoplay: true,
-            animationData: registration0,
-            rendererSettings: {
-              preserveAspectRatio: "xMidYMid slice",
-            },
-          }}
-          height={400}
-          width={400}
-          isStopped={false}
-          isPaused={false}
-        />
-      </div>
-      <div className="formCon">
-        <div className="msgCon">
-          <h1 className="typography welcomeMsg">Welcome back to Kamazon</h1>
+    <div className="loginMainCon">
+      <div className="loginSubCon">
+        <div className="sidebar left">
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: registration0,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+            }}
+            height={200}
+            width={200}
+            isStopped={false}
+            isPaused={false}
+          />
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className="form">
-          <input
-            {...register("email", { required: true })}
-            className="emailInput input"
-            placeholder="Email"
-          />{" "}
-          {/* register an input */}
-          <input
-            {...register("password", { required: true })}
-            type="password"
-            className="passwordInput input"
-            placeholder="Password"
-          />
-          <input
-            type="submit"
-            className="submitBtn "
-            value="Sign up"
-            disabled={loading}
-          />
-        </form>
-
-        <div className="footerdiv">
-          <div className="google row">
+        <div className="loginFormCon">
+          <form onSubmit={handleSubmit(onSubmit)} className="form">
+            <h3 className="welcomeMsg">
+              Welcome to
+              <h2 className="brandNameMsg"> Kamazon</h2>
+            </h3>
+            <input
+              {...register("email", { required: true })}
+              className="emailInput input"
+              placeholder="Email"
+            />{" "}
+            {/* register an input */}
+            <input
+              {...register("password", { required: true })}
+              type="password"
+              className="passwordInput input"
+              placeholder="Password"
+            />
+            <input
+              type="submit"
+              className="submitBtn "
+              value="Create an account"
+            />
             <button className="btns" onClick={() => signInWithGoogle()}>
               <img src={GoogleIcon} className="icon" />
               Sign in with google{" "}
             </button>
-          </div>
-          <div className="fb row">
-            <button className="btns" onClick={() => signInWithFacebook()}>
-              <img src={FBIcon} className="icon" />
-              Sign in with Facebook{" "}
-            </button>
-          </div>
-          <div className=" row">
             <button
-              className="btns account"
+              className="btns"
+              id="register"
               onClick={() => history.push("/login")}
             >
-              Back to login
+              Sign in
             </button>
-          </div>
+          </form>
+
+          {/* end of login form con */}
         </div>
-      </div>
-      <div className="sidebar right">
-        <Lottie
-          options={{
-            loop: true,
-            autoplay: true,
-            animationData: registration1,
-            rendererSettings: {
-              preserveAspectRatio: "xMidYMid slice",
-            },
-          }}
-          height={400}
-          width={400}
-          isStopped={false}
-          isPaused={false}
-        />
+        <div className="sidebar right">
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: registration1,
+              rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice",
+              },
+            }}
+            height={150}
+            width={150}
+            isStopped={false}
+            isPaused={false}
+          />
+        </div>
+        {/*  */}
       </div>
     </div>
+    // <div className="mainConLogin">
+    //   <div className="sidebar left">
+    //     <Lottie
+    //       options={{
+    //         loop: true,
+    //         autoplay: true,
+    //         animationData: registration0,
+    //         rendererSettings: {
+    //           preserveAspectRatio: "xMidYMid slice",
+    //         },
+    //       }}
+    //       height={400}
+    //       width={400}
+    //       isStopped={false}
+    //       isPaused={false}
+    //     />
+    //   </div>
+    //   <div className="formCon">
+    //     <div className="msgCon">
+    //       <h1 className="typography welcomeMsg">Welcome back to Kamazon</h1>
+    //     </div>
+    //     <form onSubmit={handleSubmit(onSubmit)} className="form">
+    //       <input
+    //         {...register("email", { required: true })}
+    //         className="emailInput input"
+    //         placeholder="Email"
+    //       />{" "}
+    //       {/* register an input */}
+    //       <input
+    //         {...register("password", { required: true })}
+    //         type="password"
+    //         className="passwordInput input"
+    //         placeholder="Password"
+    //       />
+    //       <input
+    //         type="submit"
+    //         className="submitBtn "
+    //         value="Sign up"
+    //         disabled={loading}
+    //       />
+    //     </form>
+
+    //     <div className="footerdiv">
+    //       <div className="google row">
+    //         <button className="btns" onClick={() => signInWithGoogle()}>
+    //           <img src={GoogleIcon} className="icon" />
+    //           Sign in with google{" "}
+    //         </button>
+    //       </div>
+    //       <div className="fb row">
+    //         <button className="btns" onClick={() => signInWithFacebook()}>
+    //           <img src={FBIcon} className="icon" />
+    //           Sign in with Facebook{" "}
+    //         </button>
+    //       </div>
+    //       <div className=" row">
+    //         <button
+    //           className="btns account"
+    //           onClick={() => history.push("/login")}
+    //         >
+    //           Back to login
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="sidebar right">
+    //     <Lottie
+    //       options={{
+    //         loop: true,
+    //         autoplay: true,
+    //         animationData: registration1,
+    //         rendererSettings: {
+    //           preserveAspectRatio: "xMidYMid slice",
+    //         },
+    //       }}
+    //       height={400}
+    //       width={400}
+    //       isStopped={false}
+    //       isPaused={false}
+    //     />
+    //   </div>
+    // </div>
   );
 };
 
